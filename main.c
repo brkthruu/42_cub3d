64b70a3c54e2bfd5fed5b36d0c1c054627a4a186
hysimok <jeonghy95@gmail.com>
2020-10-23 15:14:08 +0900

    load image and put pixels on the image

--- main.c
+++ main.c
@@ -26,12 +26,8 @@ int		main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	void	*img_ptr;
 	t_param	param;
 	t_img	img;
-
-	int		img_width;
-	int 	img_height;
 
 	int		idx_w;
 	int		idx_h;
@@ -44,21 +40,13 @@ int		main(void)
 	//2 key press
 	mlx_hook(win_ptr, X_EVENT_KEY_PRESS, 1L<<0, &key_press, &param);
 	
-	//3 load image
-	img_ptr = mlx_xpm_file_to_image(mlx_ptr, "42logo.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 150, 150);
-	
-	
-	//4 draw image
-	//new image를 생성하고 get_data_addr 함수로 해당 이미지의 주소를 받아와야 수정이 가능하다
-	
-	img.img_ptr = mlx_new_image(mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	//5 load image and put pixel
+	img.img_ptr = mlx_xpm_file_to_image(mlx_ptr, "42logo.xpm", &img.img_width, &img.img_height);
 	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
 	//여기서 (int *)를 안해주고 img.data가 void *이면 밑에 배열값들을 참조할 때 다 4를 곱해야한다.
 	//그렇기 때문에 int *로 캐스팅해주는편이 좋다고 한다.
 	
 
-	//화면을 2차원 배열로 보고 한칸씩 색을 채워나가는 코드
 	idx_h = -1;
 	while (++idx_h < IMG_HEIGHT)
 	{
@@ -72,6 +60,8 @@ int		main(void)
 		}
 	}
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img_ptr, 0, 0);
+
+
 	mlx_loop(mlx_ptr);
 
 	return (0);
