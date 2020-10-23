d11c2ab65bfd39d6ddc1a5a05f8ba707e4484091
hysimok <jeonghy95@gmail.com>
2020-10-23 15:07:10 +0900

    draw image to window

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: hjung <hjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 10:53:41 by hjung             #+#    #+#             */
-/*   Updated: 2020/10/23 11:04:51 by hjung            ###   ########.fr       */
+/*   Updated: 2020/10/23 14:53:09 by hjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,28 @@
 # define KEY_S			1
 # define KEY_D			2
 
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
+
+# define IMG_WIDTH 400
+# define IMG_HEIGHT 300
+
 typedef struct	s_param{
 	int			x;
 	int			y;
 	char		str[3];
 }				t_param;
+
+typedef struct	s_img
+{
+	void		*img_ptr;
+	int			*data;
+	//아래 3개 값은 이해 안해도 사용하는데 지장이 없음. 
+	//선언한뒤 함수의 인자로만 잘 넣어주면 알아서 정보를 받아나옴.
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
 
 
 #endif
--- main.c
+++ main.c
@@ -28,17 +28,51 @@ int		main(void)
 	void	*win_ptr;
 	void	*img_ptr;
 	t_param	param;
+	t_img	img;
 
 	int		img_width;
 	int 	img_height;
 
+	int		idx_w;
+	int		idx_h;
+
+	//1 initialization
 	param_init(&param);
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "hello world");
+	win_ptr = mlx_new_window(mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "hello world");
 	
+	//2 key press
 	mlx_hook(win_ptr, X_EVENT_KEY_PRESS, 1L<<0, &key_press, &param);
 	
+	//3 load image
 	img_ptr = mlx_xpm_file_to_image(mlx_ptr, "42logo.xpm", &img_width, &img_height);
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 150, 150);
+	
+	
+	//4 draw image
+	//new image를 생성하고 get_data_addr 함수로 해당 이미지의 주소를 받아와야 수정이 가능하다
+	
+	img.img_ptr = mlx_new_image(mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
+	//여기서 (int *)를 안해주고 img.data가 void *이면 밑에 배열값들을 참조할 때 다 4를 곱해야한다.
+	//그렇기 때문에 int *로 캐스팅해주는편이 좋다고 한다.
+	
+
+	//화면을 2차원 배열로 보고 한칸씩 색을 채워나가는 코드
+	idx_h = -1;
+	while (++idx_h < IMG_HEIGHT)
+	{
+		idx_w = -1;
+		while (++idx_w < IMG_WIDTH)
+		{
+			if (idx_w % 2)
+				img.data[idx_h * IMG_WIDTH + idx_w] = 0xFFFFFF;
+			else
+				img.data[idx_h * IMG_WIDTH + idx_w] = 0x008080;
+		}
+	}
+	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img_ptr, 0, 0);
 	mlx_loop(mlx_ptr);
+
+	return (0);
 }
