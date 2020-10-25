2405337d4e443d105d98d27945c04cda085c6c1b
hysimok <jeonghy95@gmail.com>
2020-10-25 14:42:53 +0900

    calculate perpendicular distance and adjust color

--- lodev/01_untextured_raycast.c
+++ lodev/01_untextured_raycast.c
@@ -66,6 +66,19 @@ void	param_init(t_info *info)
 	info->moveSpeed = 0.05;
 	info->rotSpeed = 0.05;
 }
+
+void	verLine(t_info *info, int x, int y1, int y2, int color)
+{
+	int	y;
+
+	y = y1;
+	while (y <= y2)
+	{
+		mlx_pixel_put(info->mlx, info->win, x, y, color);
+		y++;
+	}
+}
+
 void	calc(t_info *info)
 {
 	int		x;
@@ -89,7 +102,7 @@ void	calc(t_info *info)
 		//length of ray from one x or y-side to next x or y-side
 		double deltaDistX = fabs(1 / rayDirX);
 		double deltaDistY = fabs(1 / rayDirY);
-		double perpWallDist;
+		double perpWallDist;		//distance between perpendicular wall and player
 
 		//what direction to step in x or y-direction (either +1 or -1)
 		int		stepX;
@@ -142,6 +155,41 @@ void	calc(t_info *info)
 				hit = 1;
 		}
 		
+		//Calculate distance projected on camera direction
+		if (side == 0)
+			perpWallDist = (mapX - info->posX + (1 - stepX) / 2) / rayDirX;
+		else
+			perpWallDist = (mapY - info->posY + (1 - stepY) / 2) / rayDirY;
+		
+		//Calculate height of line to draw on screen
+		int	lineHeight = (int)(screenHeight / perpWallDist);
+
+		//Calculate lowest and highest pixel to fill in current stripe
+		int	drawStart = -lineHeight / 2 + screenHeight / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int	drawEnd = lineHeight / 2 + screenHeight / 2;
+		if (drawEnd >= screenHeight)
+			drawEnd = screenHeight - 1;
+		
+		//choose wall color
+		int	color;
+		if (worldMap[mapX][mapY] == 1)
+			color = 0xFF0000;
+		else if (worldMap[mapX][mapY] == 2)
+			color = 0x00FF00;
+		else if (worldMap[mapX][mapY] == 3)
+			color = 0x0000FF;
+		else if (worldMap[mapX][mapY] == 4)
+			color = 0xFFFFFF;
+		else
+			color = 0xFFFF00;
+
+		if (side == 1)
+			color = color / 2;
+		
+		//draw the pixels of the stripe as a vertical line
+		verLine(info, x, drawStart, drawEnd, color);
 		x++;
 	}
 	
@@ -152,6 +200,7 @@ int		main_loop(t_info *info)
 	//calculate ray position and direction
 	calc(info);
 
+	return 0;
 }
 
 int		main(int argc, char *argv[])
@@ -162,5 +211,6 @@ int		main(int argc, char *argv[])
 	info.win = mlx_new_window(info.mlx, screenWidth, screenHeight, "lodev_Raycaster");
 	
 	mlx_loop_hook(info.mlx, &main_loop, &info);
+	
 	mlx_loop(info.mlx);
 }
