92109896ec4a880abe8974ac41a4a8d52d05d063
hysimok <jeonghy95@gmail.com>
2020-10-27 20:24:28 +0900

    remove trash

--- parse_cub/parse_map.c
+++ parse_cub/parse_map.c
@@ -6,66 +6,11 @@
 /*   By: hjung <hjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 17:13:50 by hjung             #+#    #+#             */
-/*   Updated: 2020/10/27 19:55:15 by hjung            ###   ########.fr       */
+/*   Updated: 2020/10/27 20:23:45 by hjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
-
-static int	chk_map_validity_space(t_map_info *map_info, int i, int j)
-{
-	if (i == 0)
-	{
-		if (map_info->map[i][j + 1] == '0' || map_info->map[i + 1][j] == '0')
-			return (0);
-	}
-	else if (j == 0)
-	{
-		if (map_info->map[i][j + 1] == '0' || map_info->map[i + 1][j] == '0')
-			return (0);
-	}
-	else if (i == map_info->rows - 1)
-	{
-		if (map_info->map[i][j - 1] == '0' || map_info->map[i - 1][j] == '0')
-			return (0);
-	}
-	else
-	{
-		if (map_info->map[i][j - 1] == '0' || map_info->map[i - 1][j] == '0'
-			|| map_info->map[i][j + 1] == '0' || map_info->map[i + 1][j] == '0')
-			return (0);
-	}
-	return (1);
-}
-
-static int	chk_map_validity_edge(t_map_info *map_info)
-{
-	int		i;
-	int		j;
-	char	**map_buffer;
-
-	i = 0;
-	j = 0;
-	while (i < map_info->rows)
-	{
-		j = 0;
-		while (j < ft_strlen(map_info->map[i]))
-		{
-			if ((i == 0 || j == 0) && map_info->map[i][j] == '0')
-				return (0);
-			if (map_info->map[i][ft_strlen(map_info->map[i]) - 1] == '0')
-				return (0);
-			if (map_info->map[i][j] == ' ' && !chk_map_validity_space(map_info, i, j))
-			{
-				printf("error location : %d %d\n", i, j);
-				return (0);
-			}
-			j++;
-		}
-		i++;
-	}
-	return (1);
-}
 
 static void	copy_line_data(char *map_info, char *line)
 {
